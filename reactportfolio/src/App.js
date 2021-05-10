01750449cec5e2659b62b9fde42cce8802e3befc
Chris Curry <Chamilitary216@hotmail.com>
2021-05-10 15:08:30 -0400

    stlying navbar

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -2,14 +2,19 @@ import Navbar from "./components/navbar/Navbar"
 import Intro from "./components/intro/Intro"
 import Portfolio from "./components/portfolio/Portfolio"
 import Contact from "./components/contact/Contact"
+import { useState } from "react";
 import "./app.scss"
 
 
 
 function App() {
+
+  //makes menu closed on load so animation doesnt start
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar menuOpen= {menuOpen} setMenuOpen={setMenuOpen}/>
       <div className = "sections">
         <Intro />
         <Portfolio />
--- reactportfolio/src/components/navbar/Navbar.jsx
+++ reactportfolio/src/components/navbar/Navbar.jsx
@@ -1,8 +1,30 @@
 import "./Navbar.scss";
+import {Person, Mail, MenuOpen} from "@material-ui/icons"
 
-export default function Navbar() {
+export default function Navbar({ MenuOpen, setMenuOpen }) {
     return (
-        <div className = "navbar">
+        <div className = {"navbar " + (menuOpen && "active")}>
+            <div className = "wrapper">
+                <div className = "left">
+                    <a href = "#intro" className ="logo">Chris Curry Codez</a>
+                    <div className="itemContainer">
+                        <Person className ="icon" />
+                        <span>216-224-3019</span>
+
+                    </div>
+                    <div className ="itemContainer>">
+                        <Mail className ="icon" />
+                        <span>ChrisCurryCodez@gmail.com</span>
+                    </div>
+                </div>
+                <div className = "right">
+                    <div className="stacks">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
             
         </div>
     )
