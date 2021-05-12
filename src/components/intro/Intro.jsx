6b311a1c8343caa05a05cc4a3db6e03511dd3df2
Chris Curry <Chamilitary216@hotmail.com>
2021-05-12 10:44:45 -0400

    fixed case sensative files

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="images/me.png" alt="" />
+          <img src="images/Me.png" alt="" />
         </div>
       </div>
       <div className="right">
